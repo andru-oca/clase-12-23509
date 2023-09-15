71e88463b6dbbb7ae61c5cf697c154c86cb9d1a7
aocana-data <aocanatorres@buenosaires.gob.ar>
2023-09-15 17:51:43 -0300

    este es mi feature, soy caro, pay me!

--- main.cs
+++ main.cs
@@ -7,3 +7,13 @@ RUN apt update -y && apt upgrade -y && apt install git nano -y
 # docker run  --name local_ubuntu -v ./project/:/project -d -it  ubuntu
 # docker exec -it local_ubuntu bash
 
+inlcude files.cs
+
+function pirulo(data){
+    cout >> data
+}
+
+
+var:string piruloResult = pirulo()
+
+cout >> piruloResult
